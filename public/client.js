92902a4739985d512352b2b22c0aa28b5a740c4e
Ziembski <karolzurawinski@gmail.com>
2025-11-24 22:09:56 +0100

    a

--- public/client.js
+++ public/client.js
@@ -5,7 +5,11 @@ const evtSource = new EventSource("/sse");
 evtSource.onmessage = (e) => {
     latestData = JSON.parse(e.data);
 
-    if (window.updatePageValues) {
+    // Global access for delayed DOM loading
+    window.latestData = latestData;
+
+    // Call subpage update function if it exists
+    if (typeof window.updatePageValues === "function") {
         window.updatePageValues(latestData);
     }
 };

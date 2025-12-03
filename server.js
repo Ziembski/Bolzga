6f2717fe530e9bf2051ae127841f15b772da77b9
Ziembski <karolzurawinski@gmail.com>
2025-12-03 14:49:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Store the current data in memory
 let currentData = {

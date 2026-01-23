74790de94db434e09c1f3ff2cdb2ed5d854599d0
Ziembski <karolzurawinski@gmail.com>
2026-01-23 14:26:35 +0100

    Update dice.js

--- public/dice.js
+++ public/dice.js
@@ -158,7 +158,7 @@ const DICE = (function() {
         if (this.light) this.scene.remove(this.light);
         this.light = new THREE.SpotLight(vars.spot_light_color, 2.0);
         this.light.position.set(-mw / 1, mw / 1, mw * 2);
-        this.light.target.position.set(0, 0, 0);
+        this.light.target.position.set(-mw / 1, mw / 1, mw * 2);
         this.light.distance = mw * 5;
         this.light.castShadow = true;
         this.light.shadowCameraNear = mw / 10;
